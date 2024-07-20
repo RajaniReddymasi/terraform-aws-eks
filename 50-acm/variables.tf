8a831be697ccd09e1711d04a941b38aa53a10eaa
rajanireddymasi <rajani.reddymasi1@gmail.com>
2024-07-20 15:17:39 +0530

    infra project

---
+++ 50-acm/variables.tf
@@ -0,0 +1,24 @@
+variable "project_name" {
+  default = "expense"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "common_tags" {
+  default = {
+    Project = "expense"
+    Environment = "dev"
+    Terraform = "true"
+    Component = "ingress-alb"
+  }
+}
+
+variable "zone_name" {
+  default = "aadhyuar2105.online"
+}
+
+variable "zone_id" {
+  default = "Z01325919Q84GTBXEKIO"
+}
